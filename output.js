0078eed30440f730bf0b9bdc79434863249a2412
action <action@github.com>
2025-05-23 14:46:44 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri May 23 2025 14:46:00 GMT+0000 (Coordinated Universal Time)
+//Fri May 23 2025 14:46:44 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const Iili1I = new lli1li("联通安全管家任务"),
